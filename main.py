f958b168af5491d5990c0eeb9e17f01970cbffac
Elvinato <aimteso@gmail.com>
2023-02-25 20:40:29 +0400

    API upd

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import telebot
 
-bot = telebot.TeleBot('6262495766:AAGN54gw1NlhwNxrx3jiRM1bZ2NIq2V7skY')
+bot = telebot.TeleBot('6262495766:AAEjtiuPNbKbAJuNyo6kUS253Lv52WQ7Rdw')
+
 
 @bot.message_handler(command=['start'])
 def start(message):
     bot.send_message(message.chat.id, '<b>Privet</b>', parse_mode='html')
+
 
 bot.polling(none_stop=True)
