c84097d034a1424c4304d9d6da8d2aaa93c24131
Elvinato <aimteso@gmail.com>
2023-02-25 21:25:02 +0400

    update

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ import telebot
 
 bot = telebot.TeleBot('5942487246:AAGhW_0wFKbZ-GNbweypIxbs3lSXqSaYPT8')
 
-
-@bot.message_handler(command=['start'])
+@bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, '<b>Privet</b>', parse_mode='html')
-
+    bot.send_message(message.chat.id, '<b>Hello</b>', parse_mode='html')
 
 bot.polling(none_stop=True)
