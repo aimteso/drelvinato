3a264ec06bfac8a442f33fc2a596fb1cc0746abc
Elvinato <aimteso@gmail.com>
2023-02-25 19:38:51 +0400

    test

--- Responses.py
+++ Responses.py
@@ -3,16 +3,16 @@ from datetime import datetime
 def sample_responses(input_text):
     user_message = str(input_text).lower()
 
-    if user_message in ("salam", "hi", "slm", "hello",):
-        return "Salam, Sehetiniz necedi?"
+    if user_message in ("hi", "hello", "hey"):
+        return "Hello there!"
 
-    if user_message in ("sen kimsen?", "sen kimsen", "hru", "who are you", "who are you?",):
-        return "Men Sizin meslehetciniz Dr.Elvinato!"
+    if user_message in ("what is your name", "what's your name", "who are you"):
+        return "My name is Bot, nice to meet you!"
 
-    if user_message in ("time", "time?", "saat necedi", "saat necedi?",):
+    if user_message in ("time", "what time is it", "what's the time", "tell me the time"):
         now = datetime.now()
         date_time = now.strftime("%d/%m/%y, %H:%M")
 
         return str(date_time)
 
-    return "Sizi anlamadim."
+    return "Sorry, I didn't understand what you said."
